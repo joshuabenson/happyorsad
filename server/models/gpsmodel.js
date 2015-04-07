fceb0189abeebeb5d02b01585cef17d6c2473ec5
Josh Benson <bensonjosha@gmail.com>
2015-04-07 17:43:13 -0500

    adds express servers and dougs changes

--- server/models/gpsmodel.js
+++ server/models/gpsmodel.js
@@ -2,25 +2,14 @@
 // mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/josh');
-var GPSdata;
-
 
 var fetch = function(){
-    $.ajax({ 
-    url: 'http://localhost:8080',
-    type: 'GET',
-    success: function (data) {
-      GPSdata = data;
-      console.log(GPSdata);
-    },
-    error: function (data) {
-    console.error('nope');
-    }
-    });
-  };
-  setInterval(function(){fetch()}, 500);
+  $.get('/gps', function (data) {
+    initialize(JSON.parse(data));
+  });
+};
+// setInterval(function(){fetch()}, 500);
 
-  // $('body').append(GPSdata.lon);
-  // $('body').append(GPSdata.lat);
-  console.log(GPSdata);
-  
+// $('body').append(GPSdata.lon);
+// $('body').append(GPSdata.lat);
+// console.log(GPSdata);
--- server/server.js
+++ server/server.js
@@ -2,51 +2,52 @@
 var sys = require('sys');
 var net = require('net');
 var mqtt = require('mqtt');
-var static = require('node-static');
+var express = require('express');
 
+// var static = require('node-static');
 // var io = require('socket.io').listen(80);
+var app = express();
 var data;
- var defaultCorsHeaders = {
+var defaultCorsHeaders = {
     "access-control-allow-origin": "*",
     "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS", 
     "access-control-allow-headers": "content-type, accept",
     "access-control-max-age": 10 // Seconds.
- };
- var headers = defaultCorsHeaders;
- headers['Content-Type'] = "application/json";
+};
+var headers = defaultCorsHeaders;
+headers['Content-Type'] = "application/json";
+
 var serverGet = function() {
 
-var client = mqtt.connect('mqtt://test.mosquitto.org');
+  var client = mqtt.connect('mqtt://test.mosquitto.org');
 
 
- client.on('connect', function(){
- client.subscribe('owntracks/joshb123/joshiphone');
-});
+  client.on('connect', function(){
+  client.subscribe('owntracks/joshb123/joshiphone');
+  });
 
-client.on('message', function(topic, message){
+  client.on('message', function(topic, message){
   console.log(message.toString());
 
   data = message;
 
   client.end();
-});
+  });
 };
-setInterval(function(){ serverGet() }, 5000); 
 
-var file = new static.Server('./index.html');
+app.use(express.static(__dirname));
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-       
-        file.serve(request, response);
+app.get('/gps', function(req, res){
+  //get data from mqtt server and return in response
 
-        response.writeHead(200, headers);
-        response.end(data);
-    }).resume();
+  serverGet();
+  res.send(data);
+});
 
-}).listen(8080);
+app.get('/', function(req, res){
+  res.sendfile('./index.html');
+});
 
+app.listen(3000);
 
-
-  
 
