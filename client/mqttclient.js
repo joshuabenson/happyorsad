f50b0d06cd4b254cd889324c1f7af6b8cc1e3098
Josh Benson <bensonjosha@gmail.com>
2015-04-06 19:16:21 -0500

    about to add a bandaid

--- client/mqttclient.js
+++ client/mqttclient.js
@@ -1,7 +1,7 @@
 var socket = io.connect('http://localhost:5000');
     socket.on('connect', function () {
       socket.on('mqttData', function (msg) {
-        var elmarr=msg.topic.split("/");
+      
 
         console.log(msg.topic+' '+msg.payload);
         
--- server/server.js
+++ server/server.js
@@ -4,34 +4,16 @@ var mqtt = require('mqtt');
 var mqttClient = require('MQTTClient');
 var io = require('socket.io').listen(5000);
 
-var client = new mqtt.MQTTClient(1883, 'test.mosquitto.org');
+var client = mqtt.connect('mqtt://test.mosquitto.org');
 
-io.sockets.on('connection', function(socket) {
-  socket.on('subscribe', function(data) {
-    console.log('You are subscribed to' + data.topic);
-    console.log(data.lat);
-    client.subscribe(data.topic);
-  });
+
+
+
+client.on('connect', function(){
+  client.subscribe('owntracks/joshb123/joshiphone');
 });
 
-client.addListener('mqttData', function(topic, payload) {
-  sys.puts(topic+'='+payload);
-  io.sockets.emit('mqtt', {'topic':String(topic),
-    'payload':String(payload)});
-})
-
-
-// client.on('connect', function(){
-//   client.subscribe('owntracks/joshb123/joshiphone');
-// });
-
-// client.on('message', function(topic, message){
-//   console.log(message.toString());
-//   client.end();
-// })
-
-// client.on('message', function(topic, message) {
-//   console.log(message);
-//   sys.puts(topic+ '=' +message);
-//   io.sockets.in(topic).emit('mqtt',{'topic'})
-// })
+client.on('message', function(topic, message){
+  console.log(message.toString());
+  client.end();
+});
