4d6e4aa4e7460217e4bc279eaeaaa3e3fcaddba8
Josh Benson <bensonjosha@gmail.com>
2015-04-10 12:15:03 -0500

    modifies package.json, cleans code a bit

--- server/models/gpsmodel.js
+++ server/models/gpsmodel.js
@@ -1,6 +1,5 @@
 // var express = require('express'),
 // mongoose = require('mongoose');
-
 // mongoose.connect('mongodb://localhost/josh');
 
 var fetch = function(){
@@ -8,8 +7,3 @@ var fetch = function(){
     initialize(JSON.parse(data));
   });
 };
-// setInterval(function(){fetch()}, 500);
-
-// $('body').append(GPSdata.lon);
-// $('body').append(GPSdata.lat);
-// console.log(GPSdata);
--- server/server.js
+++ server/server.js
@@ -1,26 +1,13 @@
-
-var sys = require('sys');
-var net = require('net');
 var mqtt = require('mqtt');
 var express = require('express');
-
 // var static = require('node-static');
 // var io = require('socket.io').listen(80);
 var app = express();
+app.use(express.static(__dirname));
 var data;
-var defaultCorsHeaders = {
-    "access-control-allow-origin": "*",
-    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS", 
-    "access-control-allow-headers": "content-type, accept",
-    "access-control-max-age": 10 // Seconds.
-};
-var headers = defaultCorsHeaders;
-headers['Content-Type'] = "application/json";
 
 var serverGet = function() {
-
   var client = mqtt.connect('mqtt://test.mosquitto.org');
-
 
   client.on('connect', function(){
   client.subscribe('owntracks/joshb123/joshiphone');
@@ -35,11 +22,9 @@ var serverGet = function() {
   });
 };
 
-app.use(express.static(__dirname));
 
 app.get('/gps', function(req, res){
   //get data from mqtt server and return in response
-
   serverGet();
   res.send(data);
 });
@@ -47,7 +32,7 @@ app.get('/gps', function(req, res){
 app.get('/', function(req, res){
   res.sendfile('./index.html');
 });
-
+//listen on port 3000 for requests
 app.listen(3000);
 
 
