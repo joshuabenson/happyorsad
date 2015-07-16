547b07212b57f0993d540c69af2d81b0902f72cc
Josh Benson <bensonjosha@gmail.com>
2015-07-16 11:57:00 -0400

    cleans up these files indentation deletes commented old lines

--- models/gpsmodel.js
+++ models/gpsmodel.js
@@ -1,6 +1,3 @@
-// var express = require('express'),
-// mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/josh');
 
 var fetch = function(){
   $.get('/gps', function (data) {
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var mqtt = require('mqtt');
 var express = require('express');
-// var static = require('node-static');
-// var io = require('socket.io').listen(80);
 var app = express();
 app.use(express.static(__dirname));
 var data;
@@ -10,18 +8,15 @@ var serverGet = function() {
   var client = mqtt.connect('mqtt://test.mosquitto.org');
 
   client.on('connect', function(){
-  client.subscribe('owntracks/joshb123/joshiphone');
+    client.subscribe('owntracks/joshb123/joshiphone');
   });
 
   client.on('message', function(topic, message){
-  console.log(message.toString());
-
-  data = message;
-
-  client.end();
+    console.log(message.toString());
+    data = message;
+    client.end();
   });
 };
-
 
 app.get('/gps', function(req, res){
   //get data from mqtt server and return in response
@@ -32,6 +27,7 @@ app.get('/gps', function(req, res){
 app.get('/', function(req, res){
   res.sendfile('./index.html');
 });
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 
