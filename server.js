2ed70afc670d239aa7e4fe2f77bbbe5377b6a4f5
Josh Benson <bensonjosha@gmail.com>
2015-07-20 15:03:29 -0500

    animaiton smiling

--- server.js
+++ server.js
@@ -2,36 +2,34 @@ var mqtt = require('mqtt');
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
-var data;
+var context;
 
-var serverGet = function() {
+var serverGet = function(callback) {
   var client = mqtt.connect('mqtt://test.mosquitto.org');
 
   client.on('connect', function(){
     client.subscribe('owntracks/joshb123/joshiphone');
   });
-
   client.on('message', function(topic, message){
     console.log(message.toString());
-    data = message;
-    // client.end();
+    callback.call(context, message);
+  });
+  client.on('error', function(err){
+    console.log(err);
   });
 };
 
 app.get('/gps', function(req, res){
   //get data from mqtt server and return in response
-  serverGet();
-
-  if (data) {
+  context = this;
+  serverGet(function(data){
     res.send(data);
-  } else {
-    setTimeout(function(){ res.send(data) }, 300);
-  }
+  });
 });
 
-app.get('/', function(req, res){
-  res.sendfile('./index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendfile('./index.html');
+// });
 
 var port = process.env.PORT || 8080;
 app.listen(port);
--- view/index.js
+++ view/index.js
@@ -1,7 +1,5 @@
-
-
 function initialize(GPSdata) {
-  var lat = GPSdata.lat;//data from ajax request
+  var lat = GPSdata.lat;
   var lon = GPSdata.lon;
 
   var myCenter = new google.maps.LatLng(lat,lon);
@@ -13,13 +11,33 @@ function initialize(GPSdata) {
   var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
   var marker=new google.maps.Marker({
     position:myCenter,
-    icon:'joshcopy.png'
+    icon:'joshcopy.png',
+    animation: google.maps.Animation.DROP
   });
-  // var marker=new google.maps.Marker({
-  //   position:myCenter,
-  //   });
   marker.setMap(map);
-}
+var infowindow = new google.maps.InfoWindow({
+  content: convertTimestamp(GPSdata.tst)
+});
+  //this gets run when marker is clicked
+  
+google.maps.event.addListener(marker, 'animation_changed', function(){
+  setTimeout(function(){
+    marker.setIcon('joshcopy.png');
+    infowindow.close();
+  }, 1500);
+  marker.setIcon('joshhappy.png');
+  infowindow.open(marker.get('map'), marker);
+});  
+google.maps.event.addListener(marker, 'click', function(){
+  if (marker.icon==='joshcopy.png') {
+    infowindow.open(marker.get('map'), marker);
+    marker.setIcon('joshhappy.png');
+  } else {
+    infowindow.close();
+    marker.setIcon('joshcopy.png');
+  }
+});
+};
 fetch();
 setInterval(function() { fetch(); }, 8000);
 google.maps.event.addDomListener(window, 'load', initialize);
